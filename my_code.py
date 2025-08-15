322c1dc3cbde27f5006965cde0d73e5f5189e7db
KHReddy7416 <khemanth7416@gmail.com>
2025-08-15 14:44:28 +0530

    This is my first commit in dvc

--- my_code.py
+++ my_code.py
@@ -1,11 +1,23 @@
-import pandas as pd 
+import pandas as pd
 import os
 
-data ={
-    "name":["hemanth","harini","sharmila"],
-    "age":[22,23,24],
-    "city":["chennai","bangalore","hyderabad"]
+# Data
+data = {
+    "name": ["hemanth", "harini", "sharmila"],
+    "age": [22, 23, 24],
+    "city": ["chennai", "bangalore", "hyderabad"]
 }
 
-df=pd.DataFrame(data)
-df.to_csv(os.path.join("DVC_Data_Versioning/data", "sample_data.csv"), index=False)
+# Create DataFrame
+df = pd.DataFrame(data)
+
+# Directory path
+save_dir = "data"
+
+# Create directory if it doesn't exist
+os.makedirs(save_dir, exist_ok=True)
+
+# Save CSV
+df.to_csv(os.path.join(save_dir, "sample_data.csv"), index=False)
+
+print("CSV saved successfully!")
